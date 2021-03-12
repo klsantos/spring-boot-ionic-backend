18b7cf4e5d32f9ef63978ef9dd3040755dd9b534
Kleber Santos <kl1.santos@yahoo.com.br>
2021-03-12 18:16:50 -0300

    Endpoint/clientes/{id} disponivel

--- src/main/java/com/klebersantos/cursomc/domain/Cidade.java
+++ src/main/java/com/klebersantos/cursomc/domain/Cidade.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -19,8 +21,9 @@ public class Cidade implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToOne
-	@JoinColumn(name="Estado_Id")
+	@JoinColumn(name="estado_Id")
 	private Estado estado;
 
 	public Cidade() {
