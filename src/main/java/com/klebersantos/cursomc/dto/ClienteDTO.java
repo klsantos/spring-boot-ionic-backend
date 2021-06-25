ee60ae80349c9a5ad004fa5d90e26243376e98a0
Kleber Santos <kl1.santos@yahoo.com.br>
2021-06-25 12:23:00 -0300

    PUT, DELETE e GET para cliente

--- src/main/java/com/klebersantos/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/klebersantos/cursomc/dto/ClienteDTO.java
@@ -2,31 +2,29 @@ package com.klebersantos.cursomc.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import org.hibernate.validator.constraints.Length;
-import com.klebersantos.cursomc.domain.Categoria;
 
-public class CategoriaDTO implements Serializable{
+import org.hibernate.validator.constraints.Length;
+
+import com.klebersantos.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	@Length(min=5)
 	private String nome;
-	
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Email(message="Email inválido")
 	private String email;
 	
-	
-	public CategoriaDTO() {
+	public ClienteDTO() {
 	}
 	
-	public CategoriaDTO(Categoria obj) {
+	public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+		email = obj.getEmail();
 	}
 
 	public Integer getId() {
@@ -44,14 +42,14 @@ public class CategoriaDTO implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
+	} 
 	
-
+	
 }
