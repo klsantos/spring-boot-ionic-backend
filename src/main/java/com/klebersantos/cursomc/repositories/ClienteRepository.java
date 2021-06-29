0fae9ced579f82f00131ac9385003a1a31f7e563
Kleber Santos <kl1.santos@yahoo.com.br>
2021-06-28 21:42:54 -0300

    Validãção customizada: email não repetido na inserção de Cliente

--- src/main/java/com/klebersantos/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/klebersantos/cursomc/repositories/ClienteRepository.java
@@ -3,10 +3,15 @@ package com.klebersantos.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.klebersantos.cursomc.domain.Cliente;
 
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
